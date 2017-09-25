70330976183f0c699f520da05bee4bb91568ab85
Cory Lanza <corylanza@gmail.com>
2017-09-24 21:53:37 -0400

    Entity Class

--- Pillar/Pillar/CL/EntityCL.cs
+++ Pillar/Pillar/CL/EntityCL.cs
@@ -5,7 +5,56 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Pillar {
+
+    private bool active = true;
+    private Entity parent;
+    private System system;
+    private List<Entity> children = new List<children>();
+    private List<Component> components = new List<Component>();
+
+
 	public partial class Entity {
-		
+
+		public Entity(Entity parent) {
+            this.parent = parent;
+            System = parent.System;
+        }
+
+        public void PersistentUpdate(){
+            foreach(Component component in components) {
+                component.PersistentUpdate();
+            }
+            foreach(Entity child in children) {
+                child.Update();
+            }
+        }
+
+        public void Update(){
+            foreach(Component component in components) {
+                component.Update();
+            }
+            foreach(Entity child in children) {
+                child.Update();
+            }
+        }
+        
+        public void AddChild(Entity child) {
+            
+        }
+
+
+
+
+        public bool Active {
+            get { return active; }
+            set { active = value;}
+        }
+        
+        public System System {
+            get { return system; }
+            set { System = value; }
+        }
+
+
 	}
 }
