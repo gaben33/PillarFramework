ef89271302095906801a9f7c53c09451356d8c4c
Cory Lanza <corylanza@gmail.com>
2017-09-24 22:06:51 -0400

    Fixed Entity Class

--- Pillar/Pillar/CL/EntityCL.cs
+++ Pillar/Pillar/CL/EntityCL.cs
@@ -6,44 +6,26 @@ using System.Threading.Tasks;
 
 namespace Pillar {
 
-    private bool active = true;
-    private Entity parent;
-    private System system;
-    private List<Entity> children = new List<children>();
-    private List<Component> components = new List<Component>();
-
-
 	public partial class Entity {
+
+        private bool active = true;
+        private Entity parent;
+        private System system;
+        private List<Entity> children = new List<Entity>();
+        private List<Component> components = new List<Component>();
 
 		public Entity(Entity parent) {
             this.parent = parent;
             System = parent.System;
         }
-
-        public void PersistentUpdate(){
-            foreach(Component component in components) {
-                component.PersistentUpdate();
-            }
-            foreach(Entity child in children) {
-                child.Update();
-            }
-        }
-
-        public void Update(){
-            foreach(Component component in components) {
-                component.Update();
-            }
-            foreach(Entity child in children) {
-                child.Update();
-            }
-        }
         
         public void AddChild(Entity child) {
-            
+            children.Add(child);
         }
 
-
-
+        public void AddComponent(Component component){
+            components.Add(component);
+        }
 
         public bool Active {
             get { return active; }
